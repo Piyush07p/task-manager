df83c5cc9969415e0e02d1e6c93c49dc7cd162f4
Piyush07p <piyushpatel27978@gmail.com>
2024-02-09 08:00:24 +0530

    small change 1

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -13,7 +13,7 @@ console.log(activeData)
             <section className='h-[100vh] flex flex-col items-center '>
                <h1 className='text-xl my-6'>Profile page</h1>
                <div className=' flex flex-col items-center w-[100%]'>
-                    <h1 className='my-2'>Name: {JSON.parse(localStorage.getItem("userName"))}</h1>
+                    <h1 className='my-2'>Name: xyz</h1>
                     <div className='felx flex-col  rounded-md items-center border w-[80%] sm:w-[60%] md:w-[50%] p-3'>
                         <h2 className='border-b-2 p-0 my-2 text-[red]'>Your activity</h2>
                         <div>
