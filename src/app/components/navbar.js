f764362c1167036f62ff95c9b0290997864fa0d5
Piyush07p <piyushpatel27978@gmail.com>
2024-04-02 13:13:25 +0530

    ui

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -37,35 +37,35 @@ const Navbar = () => {
         <nav className='flex items-center justify-between p-2 box-border bg-[#731273] w-[100%]'>
           
             <div className='flex items-center'>
-               <p className='text-[0.8rem] sm:text-[1.2rem]'>TASKS</p>
+               <p className='text-[0.8rem] sm:text-[1.2rem]'>Tasks</p>
             </div>
             <div>
                 <ul className='flex items-center  m-2'>
-                        <li className='mx-1 sm:mx-2   text-[1rem] sm:text-[1.2rem]  '>
+                        <li className='mx-3 sm:mx-2   text-[1rem] sm:text-[1.2rem]  '>
                             <Link href="/"><FiHome style={{display:"inline-block",marginBottom:"0.3rem",marginRight:"0.2rem"}} /><span className='hidden sm:inline'>Home</span></Link>
                         </li>
                         {/* <li className='mx-1 sm:mx-2  text-[0.6rem] sm:text-[1rem]  '>
                         <Link href="/task">Add Task</Link>
                         </li> */}
-                        <li className='mx-1  sm:mx-2 text-[1rem] sm:text-[1.2rem]  '>
+                        <li className='mx-3  sm:mx-2 text-[1rem] sm:text-[1.2rem]  '>
                             <Link href="/tasklist"><GrTask style={{display:"inline-block",marginBottom:"0.3rem",marginRight:"0.2rem"}} /><span className='hidden sm:inline'>Show tasks</span></Link>
                         </li>
                         {
                             (context.currUser)?  
                            <>
-                            <li className='mx-1 sm:mx-2  text-[0.8rem] sm:text-[1rem]   font-bold '>
+                            <li className='mx-3 sm:mx-2  text-[0.8rem] sm:text-[1rem]   font-bold '>
                             <Link href="/profile"><FaRegUser style={{display:"inline-block",marginBottom:"0.3rem"}}/> {JSON.parse(localStorage.getItem("userName"))}</Link>
                             </li>
-                            <li className='mx-1 sm:mx-2  text-[1rem] sm:text-[1rem]   shadow-sm  bg-gray-900 px-2 rounded-md py-1'>
+                            <li className='mx-3 sm:mx-2  text-[0.8rem] sm:text-[1rem]   shadow-sm  bg-gray-900 px-2 rounded-md py-1'>
                             <button  onClick={doLogout}>logout</button>
                             </li>
                            </>
                            :
                            <>
-                           <li className='mx-2 text-[0.6rem] sm:text-[1rem]   bg-gray-900 px-2 rounded-md py-1'>
+                           <li className='mx-2 text-[0.8rem] sm:text-[1rem]   bg-gray-900 px-2 rounded-md py-1'>
                            <Link href="/login">Login</Link>
                            </li>
-                           <li  className='mx-2 text-[0.6rem] sm:text-[1rem]   bg-gray-900 px-2 rounded-md py-1'>
+                           <li  className='mx-2 text-[0.8rem] sm:text-[1rem]   bg-gray-900 px-2 rounded-md py-1'>
                            <Link href="/Signup">Signup</Link>
                            </li>
                           </>
