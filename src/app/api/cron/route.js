a0f09babde8eb2dacc64f5a2edb54605d4a9875d
Piyush07p <piyushpatel27978@gmail.com>
2024-05-01 17:02:08 +0530

    added createDb function to wait for db to connect

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -3,25 +3,17 @@ import { NextResponse } from "next/server";
 import { userModel } from "@/models/user";
 import { statsModel } from "@/models/taskstatsModel";
 import { taskModel } from "@/models/task";
+import { createDb } from "@/db/connectDb";
 import moment from "moment";
 
 export async function GET(request){
+
+      await createDb();
       const allUsers=await userModel.find({});
       let userid=allUsers.map((user)=>user._id)
       console.log(userid);
       let statsDataArr=[];
 
-  //     const pendingTask=taskList.length-taskCompleted;
-  // const  accuracy= (taskCompleted/taskList.length)*100
-  // const statsObj={
-  //      userId:loggedUser._id,
-  //      taskCompleted:taskCompleted,
-  //      pendingTask:pendingTask,
-  //      date:moment().format("Do MMMM YY"),
-  //      accuracy:accuracy
-
-  // }
-    
       userid.forEach(async(id,ind)=>{
          let taskData=await taskModel.find({userId:id});
          
